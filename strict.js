6ef38cd41b666e53899f76cc39b84c2d955bfd2a
alamgirweb11 <alamgirweb11@gmail.com>
2021-04-24 23:24:58 +0600

    strict mode

---
+++ strict.js
@@ -0,0 +1,7 @@
+// strict use for remove some of js silent error
+// that is the difficult for js engine to  overcome 
+"use strict" //without strict don't show any error
+function func(){
+    name = "Jhon Doe";
+}
+func();
