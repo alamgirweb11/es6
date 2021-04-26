63bef739e7784f9637f0f07b6c9c9f04cffc4133
alamgirweb11 <alamgirweb11@gmail.com>
2021-04-27 00:02:24 +0600

    variable hoisting

---
+++ variable_hoisting.js
@@ -0,0 +1,6 @@
+// work flow of variable hoisting 
+// first assign value then declare
+// exmaple of variable hoisting
+country = "Bangladesh" // assign value
+console.log(country);
+var country; // declare variable 
