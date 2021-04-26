b5daa71b332eac53cc406fbe2983192b318fe698
alamgirweb11 <alamgirweb11@gmail.com>
2021-04-26 23:17:11 +0600

    dynamic function

---
+++ dynamic_function.js
@@ -0,0 +1,15 @@
+// syntax of conventional/noraml function
+function myFunction(){
+     // write code...
+}
+
+// syntax of dynamic function 
+let func = function (){
+    // write code...
+}
+
+// use of dynamic function
+let sum = function(a,b){
+       return a+b;
+}
+console.log(sum(5,5));
