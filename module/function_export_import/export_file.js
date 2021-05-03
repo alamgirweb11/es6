611ee15fdac015b443183b7fb2e9df55654f9e8c
alamgirweb11 <alamgirweb11@gmail.com>
2021-05-03 23:35:57 +0600

    function export import

---
+++ module/function_export_import/export_file.js
@@ -0,0 +1,11 @@
+// export variable
+let car = "BMW Car";
+
+function func1(){
+     console.log("I am from func1.");
+}
+
+function func2(){
+     console.log("I am from func2.");
+}
+export{car,func1,func2};
---
+++ module/function_export_import/import_file.js
@@ -0,0 +1,5 @@
+// import the variable 
+import {car,func1,func2} from './export_file.js';
+console.log(car);
+func1();
+func2();
