265d987e79d07922c66c7604359d4ef705144d10
alamgirweb11 <alamgirweb11@gmail.com>
2021-05-03 23:56:21 +0600

    default export import

---
+++ module/deafult_export_import/export_file.js
@@ -0,0 +1,8 @@
+// default export import
+class defaultExIm{
+      getAlert(){
+           alert("I am from defaultExIm.")
+      }
+}
+
+export default defaultExIm;
---
+++ module/deafult_export_import/import_file.js
@@ -0,0 +1,5 @@
+// received default export file with other's name
+import defaultClassReceived from './export_file.js';
+// call the default classes method
+let obj = new defaultClassReceived();
+obj.getAlert();
